bf495f3fb9110eacfa2bb8c06c5222d617013803
Your Name <you@example.com>
2022-09-05 19:39:09 -0300

    corrigindo o app js

--- app.js
+++ app.js
@@ -2,4 +2,5 @@ console.log("rodando o sistema de gerneciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
 // mais um console log de teste:
 console.log("O Paulo fez esse commit sem você ver na aula!")
+console.log("Corrigindo o appjs")
 
